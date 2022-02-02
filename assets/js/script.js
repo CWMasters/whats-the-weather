e8808b1cae0fdeea1144cdc5d2e51be68c9d4e34
CWMasters <95546410+CWMasters@users.noreply.github.com>
2022-02-01 19:20:57 -0700

    search cities works and displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@
 var cityFormEl = document.querySelector("#city-form");
 var searchBarEl = document.querySelector("#searchBar");
 var forecastContainerEl = document.querySelector("#forecast-container");
-var citySearchTerm = document.querySelector("#search-city-term");
+// var citySearchTerm = document.querySelector("#search-city-term");
 // var formInput = document.querySelector(".form-input")
 
-let userInput = "Phoenix";
+
 
 
 // PHOENIX https://api.openweathermap.org/data/2.5/onecall?lat=34.048927&lon=-111.093735&exclude=minutely,hourly&appid=25e44f201a3eb157f3f0ad7495392824"
@@ -26,28 +26,52 @@ var citySubmitHandler = function(event) {
 
     // get value from input element
     var searchBar = searchBarEl.value.trim();
-
+   
     if (searchBar) {
         getForecast(searchBar);
         searchBarEl.value = "";
+        // userInput.value = "";
     } else {
         alert("Please enter a valid City");
-
     };  
 };
+
+
+// save inputs as quick links
+// var saveUserInput = function(userInput) {
+//     if(userInput.length > 10) {
+//         userInput.splice(0, 1, 2);
+//     }
+//     userInput.push(userInput);
+//     localStorage.setItem("City", json.stringify(userInput));
+// };
+
+
 
 
 // API's to display
 var getForecast = function(city) {
     // make the request to the url
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=25e44f201a3eb157f3f0ad7495392824")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=25e44f201a3eb157f3f0ad7495392824")
       .then(function(response){
           // request was successful
           if (response.ok) {
             //   console.log(response);
               response.json().then(function(data) {
-                  console.log(data);
-                  displayForecast(data, city) //ADD DATE 
+                var {lat} = data.coord;
+                var {lon} = data.coord;
+
+                fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=25e44f201a3eb157f3f0ad7495392824`)
+                .then = function(response){
+                    if (response.ok) {
+                        response.json().then(function(data) {
+                        })
+                    } else {
+                        alert("Error: " + response.statusText);
+                    }
+                }
+                console.log(data);
+                displayForecast(data, city) //ADD DATE <<<<<<<<< 
               });
           } else {
                 alert("Error: " + response.statusText);
@@ -59,31 +83,22 @@ var getForecast = function(city) {
         
 
 
-// var displayForecast = function(forecast, searchCity) {
-//     // console.log(forecast);
-//     // console.log(searchCity);
 
-//     var celcius = Math.round(parseFloat(forecast.main.temp)-273.15);
-// 	var fahrenheit = Math.round(((parseFloat(forecast.main.temp)-273.15)*1.8)+32); 
 
-    // forecastContainerEl.textContent = "";
-    // citySearchTerm.textContent = searchCity;
-
-    function displayForecast(fc) {
+    // display in browser
+    function displayForecast(fc, name) {
         
         var fahrenheit = Math.round(((parseFloat(fc.main.temp)-273.15)*1.8)+32); 
 
         document.getElementById('breakdown').innerHTML = fc.weather[0].description;
 	    document.getElementById('forecast-container').innerHTML = fahrenheit + '&deg;';
-	    document.getElementById('subtitle').innerHTML = fc.name;
+	    document.getElementById('subtitle').innerHTML = name;
         
     }
 }
 
 
-
-
-getForecast();
+// getForecast();
 
 cityFormEl.addEventListener("submit", citySubmitHandler);
 
@@ -91,34 +106,8 @@ cityFormEl.addEventListener("submit", citySubmitHandler);
 
 
 
-// function weatherBalloon( cityID ) {
-//     var key = "25e44f201a3eb157f3f0ad7495392824";
-//     fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key) 
-//     // Convert data to json 
-//     .then(function(response) {
-//         return response.json() }) 
-//     .then(function(data) {
-//         drawWeather(data);
-//     })
-//     .catch(function() {
-//       // catch any errors
-//     });
-//   }
-  
-//   window.onload = function() {
-//     weatherBalloon( 6167865 );
 
-    
-//   }
 
-//   function drawWeather( d ) {
-// 	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-// 	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
-	
-// 	document.getElementById('description').innerHTML = d.weather[0].description;
-// 	document.getElementById('temp').innerHTML = celcius + '&deg;';
-// 	document.getElementById('location').innerHTML = d.name;
-// }
 
   
   
