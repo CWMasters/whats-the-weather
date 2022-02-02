6a4216adfd6cacebb1d2f5f062f446024152290e
CWMasters <95546410+CWMasters@users.noreply.github.com>
2022-02-02 08:10:11 -0700

    displayed in browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@
 var cityFormEl = document.querySelector("#city-form");
 var searchBarEl = document.querySelector("#searchBar");
 var forecastContainerEl = document.querySelector("#forecast-container");
+var tempEl = document.querySelector("#show-temp");
+var windEl = document.querySelector("#wind")
+var humidityEl = document.querySelector("#humidity")
+var uvIndexEl = document.querySelector("#uv-index");
+
 // var citySearchTerm = document.querySelector("#search-city-term");
 // var formInput = document.querySelector(".form-input")
 
 
 
 
-// PHOENIX https://api.openweathermap.org/data/2.5/onecall?lat=34.048927&lon=-111.093735&exclude=minutely,hourly&appid=25e44f201a3eb157f3f0ad7495392824"
+// PHOENIX https://api.openweathermap.org/data/2.5/onecall?lat=34.048927&lon=-111.093735&exclude=minutely,hourly&appid=25e44f201a3eb157f3f0ad7495392824
 // SURPRISE https://api.openweathermap.org/data/2.5/onecall?lat=34.048927&lon=-111.093735&exclude=minutely,hourly&appid=25e44f201a3eb157f3f0ad7495392824
 
 
@@ -38,40 +43,46 @@ var citySubmitHandler = function(event) {
 
 
 // save inputs as quick links
-// var saveUserInput = function(userInput) {
-//     if(userInput.length > 10) {
-//         userInput.splice(0, 1, 2);
-//     }
-//     userInput.push(userInput);
-//     localStorage.setItem("City", json.stringify(userInput));
-// };
+var city = function(userInput) {
+    if(userInput.length > 10) {
+        userInput.splice(0, 1, 2);
+    }
+    userInput.push(userInput);
+    localStorage.setItem("City", json.stringify(userInput));
+};
 
 
 
 
 // API's to display
 var getForecast = function(city) {
-    // make the request to the url
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=25e44f201a3eb157f3f0ad7495392824")
-      .then(function(response){
-          // request was successful
-          if (response.ok) {
-            //   console.log(response);
-              response.json().then(function(data) {
-                var {lat} = data.coord;
-                var {lon} = data.coord;
+  // make the request to the url
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=25e44f201a3eb157f3f0ad7495392824")
+    .then(function(response){
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
 
-                fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=25e44f201a3eb157f3f0ad7495392824`)
-                .then = function(response){
-                    if (response.ok) {
-                        response.json().then(function(data) {
-                        })
-                    } else {
-                        alert("Error: " + response.statusText);
-                    }
-                }
-                console.log(data);
-                displayForecast(data, city) //ADD DATE <<<<<<<<< 
+            
+        // var for lat and lon      
+        var {lat} = data.coord;
+        var {lon} = data.coord;
+
+        
+     
+  // fetch for lat and lon api
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=25e44f201a3eb157f3f0ad7495392824`)
+    .then = function(response){
+      if (response.ok) {
+        response.json().then(function(data) {
+        })
+            } else {
+                alert("Error: " + response.statusText);    
+          }
+        }
+          console.log(data);
+          // call display function
+          displayForecast(data, city) //ADD DATE <<<<<<<<< 
               });
           } else {
                 alert("Error: " + response.statusText);
@@ -79,23 +90,39 @@ var getForecast = function(city) {
             })
             .catch(function(error) {
               alert("Unable to connect to One Call");
+
+
+
+
+            for (let i = 1; i <= 5; i++){
+                    var cardBodyDiv = document.createElement("div");
+                  
+            }
+        
+
+
+
+
         });
         
 
-
-
-
     // display in browser
-    function displayForecast(fc, name) {
+    function displayForecast(fc, name, humidity, wind, uvi) {
         
         var fahrenheit = Math.round(((parseFloat(fc.main.temp)-273.15)*1.8)+32); 
+        var wind = Math.round(fc.wind.speed);
+        var humidity = Math.round(fc.main.humidity);
+        // var uvi = Math.round(fc.current.uvi);
 
-        document.getElementById('breakdown').innerHTML = fc.weather[0].description;
-	    document.getElementById('forecast-container').innerHTML = fahrenheit + '&deg;';
+        // document.getElementById('breakdown').innerHTML = fc.weather[0].description;
 	    document.getElementById('subtitle').innerHTML = name;
-        
+	    document.getElementById('show-temp').innerHTML = fahrenheit + '&deg;';
+        document.getElementById('wind').innerHTML = wind +  'mph';
+        document.getElementById('humidity').innerHTML = humidity + '%';
+        document.getElementById('uv-index').innerHTML = uvi;
     }
 }
+
 
 
 // getForecast();
